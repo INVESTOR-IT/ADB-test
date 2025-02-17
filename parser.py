66005b7f7d2c2756a7f24de5c1121dce8eeb1620
Svyatoslav Shareko <modek@mail.ru>
2025-02-17 13:08:10 +0300

    fixing the parser

--- parser.py
+++ parser.py
@@ -1,14 +1,20 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.action_chains import ActionChains
+
+
 
 driver = webdriver.Firefox()
 driver.get("https://www.tbank.ru/invest/indexes/TIPOUS/")
-
+actions = ActionChains(driver=driver)
 
 try:
     driver.find_element(By.CSS_SELECTOR, 'button[period="year"]').click()
 
+    chart = driver.find_element(By.CLASS_NAME, 'Line__line_MHcc2')
+    actions.move_to_element(chart).perform()
 
 finally:
     __import__('time').sleep(3)
     driver.close()
+
